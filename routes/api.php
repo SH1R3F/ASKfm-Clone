242dc856ba797fd24be838808c39fb6bbbbba55c
Mahmoud Shiref <mahmoud.shiref@scandiweb.com>
2024-08-25 19:00:13 +0300

    ASK-50: Clean up and write tests

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\TranslationController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/translations/{locale}', function (string $locale) {
-    if (file_exists(lang_path("$locale.json"))) {
-        $file = File::json(lang_path("$locale.json"));
-        return response()->json($file);
-    }
-    return response()->json([]);
-});
+Route::get('/translations/{locale}', TranslationController::class);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
